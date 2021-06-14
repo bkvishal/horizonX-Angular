ace1a9efa7dfa957e6e0a16661ab19e5456ac0ef
Vishal Srivastava <omshantivishal18@gmail.com>
2021-06-14 18:40:17 +0530

    Adding dockerfile

--- src/app/core/services/Employe.ts
+++ src/app/core/services/Employe.ts
@@ -6,18 +6,4 @@ export interface Employee {
   address: string;
   designation: string
 
-  // Employee(sapId: number,
-  //   empName: string,
-  //   number: number,
-  //   country: string,
-  //   address: string,
-  //   designation: string) {
-  //   this.sapId = sapId;
-  //   this.empName = empName;
-  //   this.number = number;
-  //   this.address = address;
-  //   this.country = country;
-  //   this.designation = designation;
-
-  // }
 }
